eed639523c39aff606d096b9580f6d51193398ea
Madimetja Sethosa <madimetjasethosa@gmail.com>
2022-03-05 09:06:07 +0200

    small edits

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Icon } from '@iconify/react'
 
-export default function Contacts(props) {
+export default function Contacts (props) {
   const { contacts } = props
   return (
-    <div className='contacts'>
+    <div id='Contacts' className='contacts'>
       <h2>Contacts</h2>
       <div className='contacts-content'>
         {contacts.map((contact) => {
           return (
-            <a className='contact' key={contact.name} href={contact.link}>
+            <a className='contact' key={contact.name} href={contact.link} target='_'>
               <Icon icon={contact.icon} /> {contact.name}
             </a>
           )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
-export default function Home (props) {
+export default function Home(props) {
   const { data } = props
   return (
     <div id='Home' className='home'>
       <div className='home-content'>
+        <div className='home-content-animation' />
         <h2 id='home-name'> Hi, I am {data.name},</h2>
         <h1 id='home-description'>{data.tagLine} </h1>
-        <p id='home-details'> {data.description} </p>
-        <button>Get in touch</button>
+        <p id='home-details'> {`"${data.description}"`} </p>
+        <a href='#Contacts' className='btn'> Get in Touch </a>
         <br />
       </div>
+      <div className='home-animation' />
     </div>
   )
 }
